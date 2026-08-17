a1049c5ec08f4360423c350adb83bf69607faf1b
Noah <ndorr16@gmail.com>
2026-08-17 14:42:05 -0400

    Experiment with custom component styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import SliderDemo from './SliderDemo'
 import Home from './Home'
+import Navbar from './components/Navbar'
  
 function App() {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/slider" element={<SliderDemo />}></Route>
     </Routes>
+    </>
   )
 }
  
