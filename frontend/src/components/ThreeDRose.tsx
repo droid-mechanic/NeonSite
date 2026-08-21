24e2656d19a36a7b7e707f52101bc700dce45ce1
Noah <ndorr16@gmail.com>
2026-08-21 10:18:38 -0400

    Add emissive areas from prototype code

--- frontend/src/components/ThreeDRose.tsx
+++ frontend/src/components/ThreeDRose.tsx
@@ -1,13 +1,42 @@
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Sparkles } from "@react-three/drei";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 
 function RoseModel() {
     const { scene } = useGLTF("assets/Rose3.gltf");
+
+    const glowMesh = scene.getObjectByName("Part_2");
+
+    if (glowMesh) {
+        glowMesh.children.forEach(function (c) {
+            if (c instanceof THREE.Mesh) {
+                c.material.emissive.set("#ff22aa");
+                c.material.emissiveIntensity = 2;
+            }
+        });
+    }
+
+    const glowMesh2 = scene.getObjectByName("Part_3");
+
+    if (glowMesh2) {
+        glowMesh2.children.forEach(function (c) {
+            if (c instanceof THREE.Mesh) {
+                c.material.emissive.set("#00ff55");
+                c.material.emissiveIntensity = 2;
+            }
+        });
+    }
+
+    return <primitive object={scene} />;
 }
 
 function ThreeDScene() {
-    return <Canvas></Canvas>;
+    return (
+        <Canvas>
+            <RoseModel />
+        </Canvas>
+    );
 }
 
 export default ThreeDScene;
